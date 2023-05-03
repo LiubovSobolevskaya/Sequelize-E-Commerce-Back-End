dd90e7c816c0ba5445d18044796974e572f790fe
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-05-02 21:24:52 -0700

    add fields to models

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
+//mysql://eyyo7kgh2kslv7rf:bx4vwiisif7yfcjo@kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a4hppuw85bc1dzzw
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
